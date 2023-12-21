6a8350f840e33f25d152a2071451488549c4fed9
antonyo <antonyo.turco@gmail.com>
2023-12-21 16:35:20 +0100

    Update task_1986183.cc
    ++macros error handling e gestione input da terminale
    ++funzione checkArgs usando la metodologia vista in classe
    ++spostato l'inizializzazione dei NodeContainers in funzione createAllNodes

--- homework2/task_1986183.cc
+++ homework2/task_1986183.cc
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <cstring>
+
 #include "ns3/applications-module.h"
 #include "ns3/core-module.h"
 #include "ns3/csma-module.h"
@@ -8,118 +11,44 @@
 #include "ns3/ssid.h"
 #include "ns3/yans-wifi-helper.h"
 
-#include <stdlib.h>
-#include <cstring>
-
-// Network Topology
+// CONSOLE MESSAGES
+#define MATRICOLA_REF (std::string) "1986183"
+#define MISSING_MATRICOLA "[Error] Inserire la matricola dello studente referente come parametro. (Hint: Gruppo 25).\n"
+#define WRONG_MATRICOLA "[Error] La matricola inserita è errata. (Hint: Gruppo 25).\n"
+#define RIGHT_MATRICOLA "\tMatricola inserita corretta. Proceeding...\n"
+#define RTS_CTS_OK "\tRTS/CTS abilitato. Proceeding...\n"
+#define TRACING_OK "\tTracing promiscuo abilitato. Proceeding...\n"
+#define TOO_MANY_ARGUMENTS "[Error] Troppi argomenti (max 3).\n"
 
 using namespace ns3;
 
+bool enableRtsCts = false;
+bool tracing = false;
+bool verbose = true;
+
 NS_LOG_COMPONENT_DEFINE("Task_1986183");
 
-int main(int argc, char* argv[]){
+// Controlla gli argomenti passati alla simulazione e setta le flag correttamente
+void checkArgs(int argc, char * argv[]);
 
-    char* studentId = (char *) malloc(strlen(argv[1]));
-    studentId = argv[1];
-    
-    switch(argc) {
-        case 1:
-            std::cerr << "Inserire matricola studente come parametro d'esecuzione referente (Hint: Antonio Turco)\n";
-            exit(EXIT_FAILURE);
-            break;
-        case 2:
-            std::cout << "Matricola inserita: " << studentId << "\n";
-            if(strncmp(studentId, "1986183", strlen(studentId))!=0){
-                std::cerr << "Matricola Errata\n";
-                exit(EXIT_FAILURE);
-            }
-            break;
-        default: 
-            std::cerr << "Troppi argomenti inseriti (max 1)\n";
-            exit(EXIT_FAILURE);
-    }
-    char c;
+// Popola i NodeContainers con i nodi secondo le indicazioni progettuali
+void createAllNodes(NodeContainer *nodes, NodeContainer *p2pNodes, NodeContainer *wifiNodes, NodeContainer *csmaNodes);
 
-    bool enableRtsCts = false;
-    bool tracing = false;
+int main(int argc, char* argv[]) {
+    checkArgs(argc, argv); 
 
-    std::cout << "Forzare RtS/CtS nei pacchetti? [Y/n]\n";
-    std::cin >> c;
-    if(c=='Y' || c == 'y'){
-        enableRtsCts = true;
-    }
-    
-    std::cout << "Abilitare tracing promiscuo? [Y/n]\n";
-    std::cin >> c;
-    if(c=='Y' || c == 'y'){
-        tracing = true;
-    }
+    //Come scelta progettuale abbiamo deciso di creare:
+    NodeContainer nodes;        // un Container generale per una gestione migliore degli indici [0-19];
+    NodeContainer p2pNodes;     // un Container per nodi collegati da topologie P2P;
+    NodeContainer wifiNodes;    // un Container per i nodi collegati via topologia Wi-fi;
+    NodeContainer csmaNodes;    // un Container per i nodi collegati con protocollo CSMA.
 
-
-    std::cout << "Inizio Simulazione\n"; 
-    if(enableRtsCts) std::cout << "Rts/CtS abilitato\n";
-    if(tracing) std::cout << "tracing promiscuo abilitato\n"; 
-
-    //Come scelta progettuale abbiamo deciso di creare un Container generale per una gestione migliore degli indici [0-19]
-    // e in secondo luogo abbiamno deciso di creare dei Container specifici per nodi collegati in P2P un Container per i nodi in wifi 
-    // e un Container per i nodi in CSMA
-
-    NodeContainer nodes; // Container per tutti i nodi
-    NodeContainer p2pNodes; // Container per i nodi in P2P
-    NodeContainer wifiNodes; // Container per i nodi in Wifi
-    NodeContainer csmaNodes; // Container per i nodi in CSMA
-
-    nodes.Create(20); // Creo tutti i miei nodi 
-
-    // Aggiungo i nodi collegati con CSMA nell mio Container specifico , mi aiuto con gli indici
-    /*
-    for(int i = 0; i < 2; i++)
-        csmaNodes.Add(nodes.Get(i));
-    */
-    csmaNodes.Add(nodes.Get(0));
-    csmaNodes.Add(nodes.Get(1));
-    csmaNodes.Add(nodes.Get(2));
-
-    // Aggiungo i nodi collegati in P2P nel mio Container specifico , mi aiuto con gli indici
-    /*
-    for(int i = 2; i < 11; i++)
-        p2pNodes.Add(nodes.Get(i));
-    */
-    p2pNodes.Add(nodes.Get(2));
-    p2pNodes.Add(nodes.Get(3));
-    p2pNodes.Add(nodes.Get(4));
-    p2pNodes.Add(nodes.Get(5));
-    p2pNodes.Add(nodes.Get(6));
-    p2pNodes.Add(nodes.Get(7));
-    p2pNodes.Add(nodes.Get(8));
-    p2pNodes.Add(nodes.Get(9));
-    p2pNodes.Add(nodes.Get(10));
-
-    // Aggiungo i nodi collegati in Wifi nel mio Container specifico , mi aiuto con gli indici
-    /*
-    for(int i = 10; i < 20; i++)
-        p2pNodes.Add(nodes.Get(i));
-    */
-    wifiNodes.Add(nodes.Get(10));
-    wifiNodes.Add(nodes.Get(11));
-    wifiNodes.Add(nodes.Get(12));
-    wifiNodes.Add(nodes.Get(13));
-    wifiNodes.Add(nodes.Get(14));
-    wifiNodes.Add(nodes.Get(15));
-    wifiNodes.Add(nodes.Get(16));
-    wifiNodes.Add(nodes.Get(17));
-    wifiNodes.Add(nodes.Get(18));
-    wifiNodes.Add(nodes.Get(19));
+    createAllNodes(&nodes, &p2pNodes, &wifiNodes, &csmaNodes);
 
     //Creo un helper  per i  nodi CSMA
-    CsmaHelper csma1;
-    CsmaHelper csma2;
-
-    csma1.SetChannelAttribute("DataRate", StringValue("10Mbps"));
-    csma1.SetChannelAttribute("Delay", StringValue("200ms"));
-
-    csma2.SetChannelAttribute("DataRate", StringValue("10Mbps"));
-    csma2.SetChannelAttribute("Delay", StringValue("200ms"));
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", StringValue("10Mbps"));
+    csma.SetChannelAttribute("Delay", StringValue("200ms"));
 
     //Creo 3 per helper per i nodi P2P
     PointToPointHelper pointToPoint5;
@@ -134,8 +63,6 @@ int main(int argc, char* argv[]){
     pointToPoint10.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
     pointToPoint10.SetChannelAttribute("Delay", StringValue("200ms"));
 
-    // TODO Antonio :
-        // Creo un helper per nodi Wifi
     NodeContainer wifiApNode = p2pNodes.Get(10);
 
     YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
@@ -171,27 +98,98 @@ int main(int argc, char* argv[]){
     mobility.Install(wifiNodes);
 
     // Creo la netdevice per i CSMA
-    NetDeviceContainer devices_0_2 = csma1.Install(nodes.Get(0));
-    NetDeviceContainer devices_2_0 = csma1.Install(nodes.Get(2));
-    NetDeviceContainer devices_1_2 = csma2.Install(nodes.Get(1));
-    NetDeviceContainer devices_2_1 = csma2.Install(nodes.Get(2));
+    NetDeviceContainer devices_0 = csma.Install(nodes.Get(0));
+    NetDeviceContainer devices_1 = csma.Install(nodes.Get(1));
+    NetDeviceContainer devices_2 = csma.Install(nodes.Get(2));
 
     // Creo i netdevices per i P2P DataRate = 100Mbps Delay = 20ms
-    NetDeviceContainer devices_2_4 = pointToPoint100.Install(nodes.Get(2) , nodes.Get(4));
-    NetDeviceContainer devices_4_5 = pointToPoint100.Install(nodes.Get(4) , nodes.Get(5));
-    NetDeviceContainer devices_4_10 = pointToPoint100.Install(nodes.Get(4) , nodes.Get(10));
+    NetDeviceContainer devices_2_4 = pointToPoint100.Install(nodes.Get(2), nodes.Get(4));
+    NetDeviceContainer devices_4_5 = pointToPoint100.Install(nodes.Get(4), nodes.Get(5));
+    NetDeviceContainer devices_4_10 = pointToPoint100.Install(nodes.Get(4), nodes.Get(10));
     // DataRate = 10Mbps Delay = 200ms
-    NetDeviceContainer devices_4_3 = pointToPoint10.Install(nodes.Get(4) , nodes.Get(3));
+    NetDeviceContainer devices_4_3 = pointToPoint10.Install(nodes.Get(4), nodes.Get(3));
     // DataRate = 5Mbps Delay = 20ms
-    NetDeviceContainer devices_5_7 = pointToPoint5.Install(nodes.Get(5) , nodes.Get(7));
-    NetDeviceContainer devices_5_6 = pointToPoint5.Install(nodes.Get(5) , nodes.Get(6));
-    NetDeviceContainer devices_6_8 = pointToPoint5.Install(nodes.Get(6) , nodes.Get(8));
-    NetDeviceContainer devices_6_9 = pointToPoint5.Install(nodes.Get(6) , nodes.Get(9));
-    
-    //LogComponentEnable("UdpEchoClientApplication", LOG_FUNCTION);
-    //LogComponentEnable("UdpEchoServerApplication", LOG_FUNCTION);
+    NetDeviceContainer devices_5_7 = pointToPoint5.Install(nodes.Get(5), nodes.Get(7));
+    NetDeviceContainer devices_5_6 = pointToPoint5.Install(nodes.Get(5), nodes.Get(6));
+    NetDeviceContainer devices_6_8 = pointToPoint5.Install(nodes.Get(6), nodes.Get(8));
+    NetDeviceContainer devices_6_9 = pointToPoint5.Install(nodes.Get(6), nodes.Get(9));
 
     Simulator::Run();
     Simulator::Destroy();
     return 0;
+}
+
+void checkArgs(int argc, char * argv[]) {
+    if(argc > 5) {
+        std::cerr << TOO_MANY_ARGUMENTS;
+        exit(EXIT_FAILURE);
+    }
+    std::string matricolaInserita = "";
+    CommandLine cmd;
+    cmd.AddValue("matricola-referente", "Matricola del referente del Gruppo 25", matricolaInserita);
+    cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
+    cmd.AddValue("enable-tracing", "Abilitare il tracing promiscuo dei pacchetti", tracing);
+    cmd.AddValue("force-rts-cts", "Forzare l'uso di protocollo RTS/CTS", enableRtsCts);
+    cmd.Parse (argc, argv); 
+    if(matricolaInserita.compare((std::string) "") == 0) {
+        std::cerr << MISSING_MATRICOLA;
+        exit(EXIT_FAILURE);
+    } else if(matricolaInserita.compare(MATRICOLA_REF) != 0) {
+        std::cerr << WRONG_MATRICOLA;
+        exit(EXIT_FAILURE);
+    } else {
+        std::cout << RIGHT_MATRICOLA;
+    }
+    if(!verbose) {
+        LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+        LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+        return;
+    } 
+    if(enableRtsCts) std::cout << RTS_CTS_OK;
+    if(tracing) std::cout << TRACING_OK;  
+     
+}
+
+void createAllNodes(NodeContainer *nodes, NodeContainer *p2pNodes, NodeContainer *wifiNodes, NodeContainer *csmaNodes) {
+    nodes->Create(20); // Creo tutti i miei nodi 
+
+    // Aggiungo i nodi collegati con CSMA nell mio Container specifico , mi aiuto con gli indici
+    /*
+    for(int i = 0; i < 2; i++)
+        csmaNodes->Add(nodes->Get(i));
+    */
+    csmaNodes->Add(nodes->Get(0));
+    csmaNodes->Add(nodes->Get(1));
+    csmaNodes->Add(nodes->Get(2));
+
+    // Aggiungo i nodi collegati in P2P nel mio Container specifico , mi aiuto con gli indici
+    /*
+    for(int i = 2; i < 11; i++)
+        p2pNodes->Add(nodes->Get(i));
+    */
+    p2pNodes->Add(nodes->Get(2));
+    p2pNodes->Add(nodes->Get(3));
+    p2pNodes->Add(nodes->Get(4));
+    p2pNodes->Add(nodes->Get(5));
+    p2pNodes->Add(nodes->Get(6));
+    p2pNodes->Add(nodes->Get(7));
+    p2pNodes->Add(nodes->Get(8));
+    p2pNodes->Add(nodes->Get(9));
+    p2pNodes->Add(nodes->Get(10));
+
+    // Aggiungo i nodi collegati in Wifi nel mio Container specifico , mi aiuto con gli indici
+    /*
+    for(int i = 10; i < 20; i++)
+        p2pNodes->Add(nodes->Get(i));
+    */
+    wifiNodes->Add(nodes->Get(10));
+    wifiNodes->Add(nodes->Get(11));
+    wifiNodes->Add(nodes->Get(12));
+    wifiNodes->Add(nodes->Get(13));
+    wifiNodes->Add(nodes->Get(14));
+    wifiNodes->Add(nodes->Get(15));
+    wifiNodes->Add(nodes->Get(16));
+    wifiNodes->Add(nodes->Get(17));
+    wifiNodes->Add(nodes->Get(18));
+    wifiNodes->Add(nodes->Get(19));
 }
